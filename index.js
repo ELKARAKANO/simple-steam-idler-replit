1af2e76becc5a8318b4f777ae6a75680aa69f5c5
ELKARAKANO <123221734+ELKARAKANO@users.noreply.github.com>
2024-01-27 22:23:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,31 +19,31 @@ user.on('loggedOn', () => {
 });
 
 
-// var username2 = process.env.username2;
-//var password2 = process.env.password2;
-// var shared_secret2 = process.env.shared2;
+var username3 = process.env.username3;
+var password3 = process.env.password3;
+var shared_secret3 = process.env.shared3;
 
- //var games2 = [10 ,753 ,730 ,440 ,1850740 ,239140 ,1551360 ,1274570 ,1549180 ,304930 ,438100 ,302830 ,1782210 ,113400 ,273350 ,363970 ,1049410 ,298110 ,438740 ,1341290 ,865360 ,43110 ,939960 ,714010 ,218 ,49520 ,80 ,578080 ,939850 ,232090 ,301520 ,10 ,224260];  // Enter here AppIDs of the needed games
- //var status2 = 1;  // 1 - online, 7 - invisible
-
-
- //user2 = new steamUser();
- //user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
- //user2.on('loggedOn', () => {
- 	//if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
- 	//user2.setPersona(status2);               
- 	//user2.gamesPlayed(games2);
- //});
+ var games3 = [753 ,10];  // Enter here AppIDs of the needed games
+ var status3 = 1;  // 1 - online, 7 - invisible
 
 
-// var username3 = process.env.username3;
-// var password3 = process.env.password3;
-// var shared_secret3 = process.env.shared3;
+ user3 = new steamUser();
+ user3.logOn({"accountName": username3, "password": password3, "twoFactorCode": steamTotp.generateAuthCode(shared_secret3)});
+ user3.on('loggedOn', () => {
+ 	if (user3.steamID != null) console.log(user3.steamID + ' - Successfully logged on');
+ 	user3.setPersona(status3);               
+ 	user3.gamesPlayed(games3);
+ });
 
-// var games3 = [753 ,730 ,364]
+
+ //var username3 = process.env.username3;
+ //var password3 = process.env.password3;
+ //var shared_secret3 = process.env.shared3;
+
+ //var games3 = [753 ,730 ,364]
 
 
-// user3 = new steamUser();
+ //user3 = new steamUser();
 // user3.logOn({"accountName": username3, "password": password3, "twoFactorCode": steamTotp.generateAuthCode(shared_secret3)});
 // user3.on('loggedOn', () => {
  	//if (user3.steamID != null) console.log(user3.steamID + ' - Successfully logged on');
