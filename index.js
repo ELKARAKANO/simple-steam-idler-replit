e05ce15f49d06d8a5b8e2dd0562f8fd25848ca5f
ELKARAKANO <123221734+ELKARAKANO@users.noreply.github.com>
2024-06-15 14:40:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [10, 2923300];  // Enter here AppIDs of the needed games
+var games = [2923300];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
@@ -19,21 +19,21 @@ user.on('loggedOn', () => {
 });
 
 
-var username3 = process.env.username3;
-var password3 = process.env.password3;
-var shared_secret3 = process.env.shared3;
+//var username3 = process.env.username3;
+//var password3 = process.env.password3;
+//var shared_secret3 = process.env.shared3;
 
- var games3 = [10, 2923300];  // Enter here AppIDs of the needed games
- var status3 = 1;  // 1 - online, 7 - invisible
+// var games3 = [10, 2923300];  // Enter here AppIDs of the needed games
+ //var status3 = 1;  // 1 - online, 7 - invisible
 
 
- user3 = new steamUser();
- user3.logOn({"accountName": username3, "password": password3, "twoFactorCode": steamTotp.generateAuthCode(shared_secret3)});
- user3.on('loggedOn', () => {
- 	if (user3.steamID != null) console.log(user3.steamID + ' - Successfully logged on');
- 	user3.setPersona(status3);               
- 	user3.gamesPlayed(games3);
- });
+ //user3 = new steamUser();
+// user3.logOn({"accountName": username3, "password": password3, "twoFactorCode": steamTotp.generateAuthCode(shared_secret3)});
+// user3.on('loggedOn', () => {
+ 	//if (user3.steamID != null) console.log(user3.steamID + ' - Successfully logged on');
+ 	//user3.setPersona(status3);               
+ 	//user3.gamesPlayed(games3);
+// });
 
 
  //var username3 = process.env.username3;
