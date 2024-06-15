e527b976f2cc9f877985d5337562a82ff8965265
ELKARAKANO <123221734+ELKARAKANO@users.noreply.github.com>
2024-06-15 09:35:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var username3 = process.env.username3;
 var password3 = process.env.password3;
 var shared_secret3 = process.env.shared3;
 
- var games3 = [753 ,10];  // Enter here AppIDs of the needed games
+ var games3 = [10, 2923300];  // Enter here AppIDs of the needed games
  var status3 = 1;  // 1 - online, 7 - invisible
 
 
